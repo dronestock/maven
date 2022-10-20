4b5e321da6b38baab48a0d520bb2b3512e2fc805
storezhang <storezhang@gmail.com>
2022-10-20 22:20:58 +0800

    fix(Maven): 忽略证书

--- pkg.go
+++ pkg.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dronestock/drone"
 )
 
@@ -15,12 +17,20 @@ func (p *plugin) pkg() (undo bool, err error) {
 	// 测试
 	if p.Test {
 		args = append(args, `test`)
-	} else {
-		args = append(args, `-Dmaven.skip.test=true`)
 	}
 
 	// 打包
 	args = append(args, `package`)
+
+	// 测试参数
+	if !p.Test {
+		args = append(args, `--define`, `maven.skip.test=true`)
+	}
+	// 额外参数
+	for key, value := range p.defines() {
+		args = append(args, `--define`, fmt.Sprintf(`%s=%s`, key, value))
+	}
+
 	// 打印更多日志
 	if p.Verbose {
 		args = append(args, `-X`)
